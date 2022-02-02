6782be03410613b724f38e94282c3a8b3a540bdb
Mike <mikej.miller440@gmail.com>
2022-02-02 14:09:22 -0600

    added public folder for front end

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
+const { application } = require('express');
+const req = require('express/lib/request');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -95,6 +98,23 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
